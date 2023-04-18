c2899fee8cac10aa128d335d36274eab8a0849bd
Roman <roma.klimov10.94@gmail.com>
2023-04-18 23:43:02 +1100

    Создал прогрмму для поиска 2й цифры из 3хзначного числа

--- Show_SecondDigit/Program.cs
+++ Show_SecondDigit/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int RandonDigit( int arg1, int arg2)
+{
+    int number = new Random().Next(arg1, arg2 + 1);
+    return number;
+}
+
+int FindSecondDigit (int number)
+{
+    number = number / 10;
+    number = number % 10; 
+    return number;
+}
+
+int number = RandonDigit(1, 999);
+Console.WriteLine(number);
+int SecondDigit = FindSecondDigit(number);
+Console.WriteLine(SecondDigit);
